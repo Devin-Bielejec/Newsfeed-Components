7d600a8e047a99c1f9272ec823a3db32acde1a3d
devin-bielejec <devin.bieleje@gmail.com>
2019-08-04 20:32:44 -0400

    Almost done with article

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,7 @@
 /* This is the data we will be using to create our article components */
 /* Look over this data, then proceed to line 91*/
+
+
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
@@ -89,26 +91,52 @@ const data = [
 ];
 
 /* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
-  
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
-
     {three separate paragraph elements}
-
     <span class='expandButton'></span>
   </div>
-
   Hint: You will need to use createElement more than once here!
-
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each peice of the data object above.
-
   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
   Step 3: return the entire component.
-
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
-
 */
+
+function articleComponent(obj) {
+  const divParent  = document.createElement("div");
+  divParent.className = "article";
+  
+  const heading = document.createElement("h2");
+  heading.textContent = obj.title;
+  divParent.appendChild(heading);
+
+  const par1 = document.createElement("p");
+  par1.className = "date";
+  par1.textContent = obj.date;
+  divParent.appendChild(par1);
+
+  const paragraphs = [obj.firstParagraph, obj.secondParagraph, obj.thirdParagraph];
+
+  paragraphs.forEach( item => {
+    let par = document.createElement("p");
+    par.textContent = item;
+    divParent.appendChild(par);
+  })
+  
+  const span = document.createElement("span");
+  span.className = "expandButton";
+  divParent.appendChild(span);
+
+  span.addEventListener("toggle", divParent);
+  
+  return divParent;
+}
+
+data.map( article => {
+  const articlesDiv = document.querySelector("div.articles");
+  articlesDiv.appendChild(articleComponent(article));
+})
+
